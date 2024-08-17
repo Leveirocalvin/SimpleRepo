f5cf0055c6ec8f4f3da02294bd2ba366f73f6db4
Calvin Leveiro <leveirocalvin550@gmail.com>
2024-08-17 16:43:08 +0530

    first commmit

---
+++ src/eCommerce/e2eTest.java
@@ -0,0 +1,32 @@
+package eCommerce;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class e2eTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
+		
+		driver.findElement(By.id("userEmail")).sendKeys("anshika@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Iamking@000");
+		driver.findElement(By.id("login")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+		
+		//continue from lecture 156
+		
+		
+	}
+}
